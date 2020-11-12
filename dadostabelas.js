58d406493242859f6fb4ec6b7a56d0b6ca6bfe13
isidro <isidro@ua.pt>
2020-11-12 22:03:21 +0000

    nova publicação

---
+++ dadostabelas.js
@@ -0,0 +1,127 @@
+var mydata = [{
+    "id":0,
+    "name": "Aquafire",
+    "price": "23€"
+},
+{
+    "id":1,
+    "name": "Letter Vision",
+    "price": "21€"
+},{
+    "id":2,
+    "name": "Flying Tortoise",
+    "price": "34€"
+},{
+    "id":3,
+    "name": "Evaflox",
+    "price": "26€"
+},{
+    "id":4,
+    "name": "Ginger Vitro",
+    "price": "3€"
+},{
+    "id":5,
+    "name": "Guano Ameretto",
+    "price": "83€"
+},{
+    "id":6,
+    "name": "Herbalance",
+    "price": "26€"
+},{
+    "id":7,
+    "name": "Isoflux",
+    "price": "73€"
+},{
+    "id":8,
+    "name": "Know Boat",
+    "price": "34€"
+},{
+    "id":9,
+    "name": "JuiceMaster",
+    "price": "43€"
+},{
+    "id":10,
+    "name": "Meteor Media",
+    "price": "234€"
+},{
+    "id":11,
+    "name": "MadRush",
+    "price": "26€"
+},{
+    "id":12,
+    "name": "Letter Vision",
+    "price": "65€"
+},{
+    "id":13,
+    "name": "Neptide",
+    "price": "76€"
+},{
+    "id":14,
+    "name": "Opportunity Spam",
+    "price": "23€"
+},{
+    "id":16,
+    "name": "AquafiOulu Rocinantere",
+    "price": "23€"
+},{
+    "id":17,
+    "name": "Portalib",
+    "price": "23€"
+},{
+    "id":18,
+    "name": "Quarx",
+    "price": "23€"
+},{
+    "id":19,
+    "name": "Pheromonia",
+    "price": "23€"
+},{
+    "id":20,
+    "name": "Reverse Curse",
+    "price": "23€"
+},{
+    "id":21,
+    "name": "Pigzart",
+    "price": "23€"
+},{
+    "id":22,
+    "name": "SlimAll",
+    "price": "23€"
+},{
+    "id":23,
+    "name": "SlumberBed",
+    "price": "23€"
+},{
+    "id":24,
+    "name": "Spoon Bait",
+    "price": "23€"
+},{
+    "id":25,
+    "name": "Teledrone",
+    "price": "23€"
+},{
+    "id":26,
+    "name": "Tofu Magician",
+    "price": "23€"
+},{
+    "id":27,
+    "name": "VegiGate",
+    "price": "23€"
+},{
+    "id":28,
+    "name": "Wear Wolf",
+    "price": "23€"
+},{
+    "id":29,
+    "name": "Water Rabbit",
+    "price": "23€"
+}
+];
+
+
+
+$(function(){
+    $('#mytable').bootstrapTable({
+        data: mydata
+    });
+});
