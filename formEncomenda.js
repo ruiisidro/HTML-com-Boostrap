ceee86ab982a02a09a30aeb8bb3e603234caa1d2
isidro <isidro@ua.pt>
2020-11-25 22:15:17 +0000

    adicionada pagina com sparklines!

---
+++ formEncomenda.js
@@ -0,0 +1,43 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+
+    //Evento change do campo Preço unitário
+    $('.price').change(function () {
+        
+
+        //Pesquisa o tr da linha do controlo alterado
+        var x = $(this).parentsUntil("tbody", "tr");
+
+        //pesquisa o controlo .qtd  na linha previamente seleccionada
+        var qtd = x.find(".qtd");
+        
+        //pequisa o controlo .total na linha previmente seleccionada
+        var total = x.find(".total");
+
+        //atribui valor à seleccção 
+        total.val($(this).val() * qtd.val());
+    });
+     
+
+})();
+
+
+function calcular(event) {
+    debugger;
+    var aux = 0;
+    $(".total").each(function () {
+
+        if ($.isNumeric($(this).val())) {
+            aux += Number($(this).val());
+        }
+
+    });
+
+    $("#total").text(aux);
+
+}
+
+function totalLinhas(){
+    alert($("#table tr").length);
+}
+ 
---
+++ produtos.js
@@ -0,0 +1,15 @@
+[
+    {
+        "qtd": 10,
+        "item": "produto c",
+        "preco": 192.34
+    },{
+        "qtd": 10,
+        "item": "produto c",
+        "preco": 192.34
+    },{
+        "qtd": 10,
+        "item": "produto c",
+        "preco": 192.34
+    }
+]
